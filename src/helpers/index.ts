9014f8221a70617302af78b0e0a73c467d91f2b4
barosanuemailtest <barosanu.email.test@gmail.com>
2020-10-03 15:35:22 +0300

    Test stable state

---
+++ src/helpers/index.ts
@@ -0,0 +1,5 @@
+export function getLetterMatchCount(guessedWord: string, secretWord: string): number {
+    const secretLetters = secretWord.split('');
+    const guessedLetterSet = new Set(guessedWord);
+    return secretLetters.filter(letter => guessedLetterSet.has(letter)).length;
+}
